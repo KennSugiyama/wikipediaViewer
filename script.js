90049de14114a5072ec3a303599707082e580cfe
Kenn Sugiyama <kenn@kbsugiyama.com>
2017-11-02 13:11:55 -0700

    Seperate out script and css into own files.

--- script.js
+++ script.js
@@ -1,9 +1,44 @@
 $('document').ready(function(){
 
-})
+  $('form').on('submit', (e) => {
+    e.preventDefault();
+    getArticle($('#searchTerm').val())
+  })
 
+  function getArticle(searchTerm) {
+    let endPoint = 'https://en.wikipedia.org/w/api.php?'
+    let apiCall = 'action=opensearch&format=json&namespace=0&limit=10&search='
+    let url = endPoint + apiCall + searchTerm
 
-// a
+    $.ajax({
+      url: url,
+      dataType: 'jsonp',
+      success: function(json){
+        parse(json)
+      }
+    });
 
+    function parse(json){
+      var articles = [];
+      for (let i = 0; i < json[1].length; i++){
+        articles.push({
+          title: json[1][i],
+          description: json[2][i],
+          link: json[3][i]
+        })
+      };
+      display(articles);
+    }
 
-// https://en.wikipedia.org/?curid=1519830
+    function display(articles){
+      for(let i = 0; i < articles.length; i++){
+        let html = `<div class="entry">
+                      <a href=${articles[i].link} target='_blank'>
+                      <h2>${articles[i].title} </h2>
+                      <p> ${articles[i].description} </p></a>`
+        $('#searchResults').append(html)
+      }
+    }
+  }
+
+})//document ready
